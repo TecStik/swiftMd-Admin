6cd2cd8265722cb84619490c2060d51c46ef41a8
Syed Muhemin Ali <149074986+muhemin123@users.noreply.github.com>
2023-11-15 00:11:53 -0800

    Patient Details Done
    Patient Details Done

--- src/Pages/PatientDetails/PatientDetails.jsx
+++ src/Pages/PatientDetails/PatientDetails.jsx
@@ -1,16 +1,38 @@
 import { Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr } from '@chakra-ui/react'
-import React, { useState } from 'react';
-import PatientData from "./Patient.json";
+import React, { useState, useEffect } from 'react';
+// import PatientData from "./Patient.json";
 import { FiEdit } from "react-icons/fi";
 import PaginationComponent from '../../Components/Pagination';
 import ClinicStyle from "./PatientDetail.module.css";
+import axios from 'axios';
+import { Url } from "../../Components/core/index";
+import CircularProgress from '@mui/material/CircularProgress';
 
-let itemsPerPage = 2;
+
+
+let itemsPerPage = 4;
 
 const PatientDetails = () => {
 
-  const [data, setDat] = useState(PatientData);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   console.log(data)
+
+  useEffect(() => {
+    axios({
+      method: "post",
+      url: Url + "/filteredPatients",
+      data: {
+        filter: {
+
+        }
+      }
+    }).then((res) => {
+      setLoading(false);
+      console.log("Patient Details", res?.data);
+      setData(res?.data);
+    }).catch(err => console.log(err?.message))
+  }, [])
 
 
   const [page, setPage] = useState(1);
@@ -35,34 +57,40 @@ const PatientDetails = () => {
       <div className={ClinicStyle.heading}>
         <h3>Patient Details</h3>
       </div>
-      <TableContainer>
-        <Table variant='striped' colorScheme='teal'>
-          <Thead>
-            <Tr>
-              <Th>Name</Th>
-              <Th>Phone</Th>
-              <Th>MRN</Th>
-              <Th>Address</Th>
-              <Th>Email</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {
-              displayedData?.map((elm) => (
+      {
+        loading ? <div style={{ display: 'flex', justifyContent: "center", alignItems: 'center', marginTop: '30px' }}>
+          <CircularProgress />
+        </div> : <>
+          <TableContainer>
+            <Table variant='striped' colorScheme='teal'>
+              <Thead>
                 <Tr>
-                  <Td>{elm?.name}</Td>
-                  <Td>{elm?.Phone}</Td>
-                  <Td>{elm?.MRN}</Td>
-                  <Td>{elm?.Address}</Td>
-                  <Td>{elm?.Email}</Td>
+                  <Th>Id</Th>
+                  <Th>Patient Name</Th>
+                  <Th>Patient Number</Th>
+                  <Th>Patient MRNumber</Th>
+                  <Th></Th>
                 </Tr>
-              ))
-            }
-          </Tbody>
-        </Table>
-      </TableContainer>
+              </Thead>
+              <Tbody>
+                {
+                  displayedData?.map((elm) => (
+                    <Tr key={elm?._id}>
+                      <Td>{elm?._id}</Td>
+                      <Td>{elm?.PatientName}</Td>
+                      <Td>{elm?.PatientNumber}</Td>
+                      <Td>{elm?.PatientMRNumber}</Td>
+                      <Td><FiEdit style={{ cursor: "pointer" }} /></Td>
+                    </Tr>
+                  ))
+                }
+              </Tbody>
+            </Table>
+          </TableContainer>
 
-      <PaginationComponent totalPages={totalPages} onChange={handlePageChange} page={page} />
+          <PaginationComponent totalPages={totalPages} onChange={handlePageChange} page={page} />
+        </>
+      }
     </div>
   )
 }
@@ -88,158 +116,3 @@ export default PatientDetails
 
 
 
-
-
-
-// import React, { useState } from 'react';
-// import PatientData from "./Patient.json";
-// import ClinicStyle from "./PatientDetail.module.css";
-// import { FiEdit } from "react-icons/fi";
-// import PaginationComponent from '../../Components/Pagination';
-// import { Link } from "react-router-dom";
-// // modal import here
-// import {
-//   Modal,
-//   ModalOverlay,
-//   ModalContent,
-//   ModalHeader,
-//   ModalFooter,
-//   ModalBody,
-//   ModalCloseButton,
-//   useDisclosure,
-//   FormControl,
-//   FormLabel,
-//   Input,
-//   Button,
-// } from '@chakra-ui/react'
-
-
-
-// const itemsPerPage = 2;  //pagination limit here
-
-
-// const PatientDetails = () => {
-//   const { isOpen, onOpen, onClose } = useDisclosure()
-
-//   const initialRef = React.useRef(null)
-//   const finalRef = React.useRef(null)
-
-//   const [data, setDat] = useState(PatientData);
-//   console.log(data)
-
-//   const [page, setPage] = useState(1);
-//   const totalPages = Math.ceil(data.length / itemsPerPage);
-//   console.log(data)
-
-
-//   // pagination handle function
-
-//   const handlePageChange = (event, value) => {
-//     setPage(value);
-//   };
-
-//   const displayedData = data.slice(
-//     (page - 1) * itemsPerPage,
-//     page * itemsPerPage
-//   );
-
-
-//   return (
-//     <div className={ClinicStyle.container}>
-//       <div className={ClinicStyle.heading}>
-//         <h3>Patient Information</h3>
-//       </div>
-//       <table className={ClinicStyle.table}>
-//         <thead className={ClinicStyle.thead_bg}>
-//           <tr className={ClinicStyle.thead_bg}>
-//             <th>Name</th>
-//             <th>Phone</th>
-//             <th>MRN</th>
-//             <th>Address</th>
-//             <th>Email</th>
-//             <th></th>
-//           </tr>
-//         </thead>
-
-//         <tbody>
-//           {
-//             displayedData && displayedData?.map((elm) => (
-//               <tr key={elm?.id}>
-//                 <td>{elm?.name}</td>
-//                 <td>{elm?.Phone}</td>
-//                 <td>{elm?.MRN}</td>
-//                 <td>{elm?.Address}</td>
-//                 <td>{elm?.Email}</td>
-//                 <td onClick={onOpen}>
-//                   <FiEdit style={{ cursor: 'pointer', margin: '0px 10px' }} />
-//                 </td>
-//               </tr>
-//             ))
-//           }
-//         </tbody>
-//       </table>
-
-//       {/* modal start */}
-
-//       <Modal
-//         initialFocusRef={initialRef}
-//         finalFocusRef={finalRef}
-//         isOpen={isOpen}
-//         onClose={onClose}
-//       >
-//         <ModalOverlay />
-//         <ModalContent>
-//           <ModalHeader>Update Patient Information</ModalHeader>
-//           <ModalCloseButton />
-//           <ModalBody pb={6}>
-//             <FormControl>
-//               <FormLabel>Name</FormLabel>
-//               <Input ref={initialRef} placeholder='Name' />
-//             </FormControl>
-
-//             <FormControl mt={4}>
-//               <FormLabel>Phone</FormLabel>
-//               <Input placeholder='Phone' />
-//             </FormControl>
-
-//             <FormControl mt={4}>
-//               <FormLabel>MRN</FormLabel>
-//               <Input placeholder='MRN' />
-//             </FormControl>
-
-
-//             <FormControl mt={4}>
-//               <FormLabel>Address</FormLabel>
-//               <Input placeholder='Address' />
-//             </FormControl>
-
-//             <FormControl mt={4}>
-//               <FormLabel>Email</FormLabel>
-//               <Input placeholder='Email' />
-//             </FormControl>
-
-//           </ModalBody>
-
-//           <ModalFooter>
-//             <Button colorScheme='blue' mr={3}>
-//               Update
-//             </Button>
-//             <Button onClick={onClose}>Cancel</Button>
-//           </ModalFooter>
-//         </ModalContent>
-//       </Modal>
-
-
-
-//       {/* modal end */}
-
-
-
-
-//       {/* pagination component here */}
-//       <PaginationComponent totalPages={totalPages} onChange={handlePageChange} page={page} />
-//     </div >
-//   )
-// }
-
-// export default PatientDetails
